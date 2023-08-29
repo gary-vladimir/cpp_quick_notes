cf12b6207ca1543f909316140c3d04d297690eba
gary-vladimir <gary@garybricks.com>
2023-08-29 14:37:03 -0600

    added versions of binary search

--- binary_search_pro.cpp
+++ binary_search_pro.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int binary_search(vector<int> &arr, int n) {
+int binary_search_Joacru(vector<int> &arr, int n) {
   int a = -1;
   int b = arr.size();
   while (b - a > 1) {
@@ -12,5 +12,38 @@ int binary_search(vector<int> &arr, int n) {
       b = mid;
   }
   if (arr[b] == n) return b;
+  return -1;
+}
+// make sure all of the functions are correct and produce the correct output against all sort of edge cases. test all functions. 
+int binary_search_Dani(vector<int> &arr, int x){
+  int a = 0, b = arr.size()-1;
+  while(a <= b){
+    int mid = a + (b - a)/2;
+    if(arr[mid] == x)return mid;
+    if(arr[mid] < x)a = mid+1;
+    else b = mid-1;
+  }
+  return -1;
+}
+
+int lower_bound(vector<int> &arr, int x){
+  int a = 0, b = arr.size();
+  while(a < b){
+    int mid = a + (b - a)/2;
+    if(arr[mid] < x)a = mid+1;
+    else b = mid;
+  }
+  if(arr[a] == x)return a;
+  return -1;
+}
+
+int upper_bound(vector<int> &arr, int x){
+  int a = 0, b = arr.size();
+  while(a < b){
+    int mid = a + (b - a)/2;
+    if(arr[mid] <= x)a = mid+1;
+    else b = mid;
+  }
+  if(arr[a] > x)return a;
   return -1;
 }
